b72180880fc4d0075bc72bd150314827d0d1ae8a
cesar <cfloressuazo@gmail.com>
2019-10-14 01:43:12 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Spike DS challenge, predicting climate change events through historical data',
+    author='Cesar Flores',
+    license='MIT',
+)
